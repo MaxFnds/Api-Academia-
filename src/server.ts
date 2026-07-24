4f0195070ff015117b46cf8eec1c2a192c22f3d6
weydl <weydlll2606@gmail.com>
2026-07-24 19:21:54 +0000

    adiciona ponto de entrada e inicialização do servidor

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+// Ponto de entrada da aplicação: importa o app já configurado e sobe o servidor HTTP.
+import app from "./app";
+
+const PORTA = process.env.PORT || 3000;
+
+app.listen(PORTA, () => {
+  console.log(`  FitWeb rodando em http://localhost:${PORTA}`);
+});
